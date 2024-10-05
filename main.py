58f63c26e289208861450687559976f495bba22c
Merlin Migan <slavikzaya@gmail.com>
2024-10-05 21:54:57 +0200

    feature(add): Ajout de route /posts/{id} du type GET, permettant de récupérer un post en fonction de son id !

--- main.py
+++ main.py
@@ -18,20 +18,30 @@ my_posts = [
    }
 ]
 
+# Définition du modèle auquel doit répondre un post !
 class Post(BaseModel):
    title: str
    content: str
    published: bool = True
    rating: Optional[int] = None
 
+# Route racine / !
 @app.get("/")
 async def root():
    return {"message": "Welcome to my API !"}
 
+# Route /posts permettant d'obtenir tous les posts !
 @app.get("/posts")
 def get_posts():
    return {"data": my_posts}
 
+# Route /posts/{id} permettant d'obtenir un post en fonction de son id !
+@app.get("/posts/{id}")
+def get_post(id):
+   print(id)
+   return {"post_detail": f"Here is post {id}"}
+
+# Route /posts permettant de créer un post !
 @app.post("/posts")
 def create_post(post_data: Post):
    # print(post_data)
